f8a44df889c8676a754fe8abe8964ca34a19da5e
lipengcheng01 <lipengcheng01@jiuqi.com.cn>
2019-12-13 17:27:52 +0800

    first commit

---
+++ src/main/java/com/lpc/myfirstspringbootproject/MyfirstspringbootprojectApplication.java
@@ -0,0 +1,13 @@
+package com.lpc.myfirstspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstspringbootprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyfirstspringbootprojectApplication.class, args);
+    }
+
+}
